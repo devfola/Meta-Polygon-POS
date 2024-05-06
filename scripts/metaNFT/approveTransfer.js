88d509a915d11ce8c18da4ecfe78d49ad72f6506
intelliDean <intelliDean@gmail.com>
2024-05-06 12:08:27 +0100

    done with project

--- scripts/metaNFT/approveTransfer.js
+++ scripts/metaNFT/approveTransfer.js
@@ -2,39 +2,39 @@ const hre = require("hardhat");
 require("dotenv").config();
 const fxERC721RootContractABI = require("../../ERC721FxRootContractABI.json");
 
-const { BRIDGE_ADDRESS, CONTRACT_ADDRESS, fxERC721RootAddress } = process.env;
+const {BRIDGE_ADDRESS, CONTRACT_ADDRESS, fxERC721RootAddress} = process.env;
 
 async function main() {
-  const [deployer] = await hre.ethers.getSigners();
-  console.log("Deploying contracts with the account:", deployer);
+    const [deployer] = await hre.ethers.getSigners();
 
-  const MetaNFT = await hre.ethers.getContractFactory("MetaNFT");
+    const MetaNFT = await hre.ethers.getContractFactory("MetaNFT");
 
-  const metaNFT = await MetaNFT.attach(CONTRACT_ADDRESS);
+    const metaNFT = await MetaNFT.attach(CONTRACT_ADDRESS);
 
-  const fxRootContract = await hre.ethers.getContractAt(
-    fxERC721RootContractABI,
-    fxERC721RootAddress
-  );
+    const fxRootContract = await hre.ethers.getContractAt(
+        fxERC721RootContractABI,
+        fxERC721RootAddress
+    );
 
-  // Approve NFTs for transfer
-  const approveTx = await metaNFT.connect(deployer).setApprovalForAll(fxERC721RootAddress, true);
+    // Approve NFTs for transfer
+    const approveTx = await metaNFT.connect(deployer).setApprovalForAll(fxERC721RootAddress, true);
 
-  await approveTx.wait();
+    await approveTx.wait();
 
-  console.log("NFT approval confirmed");
+    console.log("NFT approval confirmed");
 
-  // Deposit NFTs to Polygon Mumbai bridge
-  for (let i = 0; i < 5; i++) {
-    const depositTx = await fxRootContract.connect(deployer).deposit(metaNFT.address, BRIDGE_ADDRESS, i, "0x6566");
+    // Deposit NFTs to Polygon Mumbai bridge
+    for (let i = 0; i < 5; i++) {
 
-    await depositTx.wait();
-  }
+        const depositTx = await fxRootContract.connect(deployer).deposit(metaNFT.address, BRIDGE_ADDRESS, i, "0x6566");
 
-  console.log("NFT deposited on Polygon Mumbai");
+        await depositTx.wait();
+    }
+
+    console.log("NFT deposited on Polygon Mumbai");
 }
 
 main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
+    console.error(error);
+    process.exitCode = 1;
 });
