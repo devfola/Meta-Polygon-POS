f93f9b9577f93b70e051839b44195f099b46decc
Warith Adebowale <folarinjames.dev@gmail.com>
2024-07-16 19:58:14 +0000

    done with project

--- hardhat.config.js
+++ hardhat.config.js
@@ -6,22 +6,22 @@ const { URL, PRIVATE_KEY, ETHERSCAN_API_KEY } = process.env;
 module.exports = {
   solidity: "0.8.20",
   networks: {
-    mumbai: {
-      url: "https://rpc-mumbai.maticvigil.com",
-      accounts: [`0x${PRIVATE_KEY}`],
-    },
-    sepolia: {
-      url: URL,
-      accounts: [`0x${PRIVATE_KEY}`],
-    },
+    // mumbai: {
+    //   url: "https://rpc-mumbai.maticvigil.com",
+    //   accounts: [`0x${PRIVATE_KEY}`],
+    // },
+    // sepolia: {
+    //   url: URL,
+    //   accounts: [`0x${PRIVATE_KEY}`],
+    // },
     amoy: {
       url: "https://rpc-amoy.polygon.technology/",
       accounts: [`0x${PRIVATE_KEY}`],
     },
-    goerli: {
-      url: "https://ethereum-goerli.publicnode.com",
-      accounts: [process.env.PRIVATE_KEY],
-    },
+    // goerli: {
+    //   url: "https://ethereum-goerli.publicnode.com",
+    //   accounts: [process.env.PRIVATE_KEY],
+    // },
   },
   etherscan: {
     apiKey: ETHERSCAN_API_KEY,
