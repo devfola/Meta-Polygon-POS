c11cb844544c1804dbb8f75a3e72d20f294288a9
Warith Adebowale <folarinjames.dev@gmail.com>
2024-07-31 13:59:21 +0000

    correction done

--- scripts/metaNFT/approveTransfer.js
+++ scripts/metaNFT/approveTransfer.js
@@ -2,11 +2,10 @@ const hre = require("hardhat");
 require("dotenv").config();
 const fxERC721RootContractABI = require("../../ERC721FxRootContractABI.json");
 
-const {
-  BRIDGE_ADDRESS,
-  CONTRACT_ADDRESS,
-  fxERC721RootAddress,
-} = process.env;
+const { ACCOUNT_ADDRESS, CONTRACT_ADDRESS } = process.env;
+
+fxERC721RootAddress = "0x9e688939cb5d484e401933d850207d6750852053";
+
 
 async function main() {
   const [deployer] = await hre.ethers.getSigners();
@@ -18,22 +17,23 @@ async function main() {
   // Approve NFTs for transfer
   const approveTx = await metaNFT
     .connect(deployer)
-    .setApprovalForAll(CONTRACT_ADDRESS, true);
+    .setApprovalForAll(fxERCdress, true);
 
   await approveTx.wait();
 
   console.log("Approval completed for the 5 NFTs");
 
-   const fxRootContract = await hre.ethers.getContractAt(
-     fxERC721RootContractABI,
-     fxERC721RootAddress
-   );
+  const fxRootContract = await hre.ethers.getContractAt(
+    fxERC721RootContractABI,
+    fxERC721RootAddress
+  );
 
   // Deposit NFTs to Polygon bridge
   for (let i = 0; i < 5; i++) {
-    const depositTx = await fxRootContract.connect(deployer).deposit(metaNFT.address, BRIDGE_ADDRESS, i, "0x6566");
+    const depositTx = await fxRootContract
+      .connect(deployer)
+      .deposit(CONTRACT_ADDRESS, ACCOUNT_ADDRESS, i, "0x6566");
     await depositTx.wait();
-
   }
 
   console.log("NFT transfterred to Polygon successfully");
--- scripts/metaNFT/getBalance.js
+++ scripts/metaNFT/getBalance.js
@@ -3,11 +3,13 @@ require("dotenv").config();
 
 const { ACCOUNT_ADDRESS, CONTRACT_ADDRESS } = process.env;
 
+const contractOnAmoy = "0x44F8f3b7b5F49cBEaDc517cd1a7a67b3EF607328";
+
 async function main() {
   const MetaNFT = await hre.ethers.getContractFactory("MetaNFT");
-  const metaNFT = await MetaNFT.attach(CONTRACT_ADDRESS);
+  const nftContractOnAmoy = await MetaNFT.attach(contractOnAmoy);
 
-  const balance = await metaNFT.balanceOf(ACCOUNT_ADDRESS);
+  const balance = await nftContractOnAmoy.balanceOf(ACCOUNT_ADDRESS);
 
   console.log("Balance: ", balance.toString());
 }
